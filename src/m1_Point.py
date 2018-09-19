280f053cdb49ccfa48282b66fb2ef554bf2fe5d4
Loco9 <codylj@rose-hulman.edu>
2018-09-19 11:03:41 -0400

    DONE

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -69,7 +69,12 @@ class Point(object):
         return self.total
 
     def get_distance_from(self, p1):
-        
+        import math
+        distance = math.sqrt(((self.x - p1.x) ** 2) + (self.y - p1.y) ** 2)
+        return distance
+
+    def get_distance_from_start(self):
+
 
 def run_test_init():
     """
@@ -795,7 +800,7 @@ def run_test_get_distance_from_start():
         print('Actually is:', p2.get_distance_from_start())
     """
     # --------------------------------------------------------------------------
-    # TODO: 10.  Follow the same instructions as in _TODO_ 3 above,
+    # DONE: 10.  Follow the same instructions as in _TODO_ 3 above,
     #    but for the  get_distance_from_START  method specified above.
     # --------------------------------------------------------------------------
     print()
@@ -804,6 +809,35 @@ def run_test_get_distance_from_start():
     print('of the Point class.')
     print('-----------------------------------------------------------')
 
+    p1 = Point(20, 30)
+    p1.move_to(111, 222)
+    p1.move_by(10, 20)
+    p1.move_to(0, 0)
+    p1.move_to(21, 31)
+    print()
+    print('p1 from start to (21, 31), should be about 1.414')
+    print('Actually is:', p1.get_distance_from_start())
+
+    p1.move_by(29, 39)
+    print()
+    print('p1 from start to (50, 70), should be about 50.0')
+    print('Actually is:', p1.get_distance_from_start())
+
+    p2 = Point(1, 1)
+    print()
+    print('p2 from start to (1, 1), should be about 0.0')
+    print('Actually is:', p2.get_distance_from_start())
+
+    p2.move_to(11, 1)
+    print()
+    print('p2 from start to (11, 1), should be about 10.0')
+    print('Actually is:', p2.get_distance_from_start())
+
+    p2.move_to(999, 999)
+    p2.move_to(1, 1)
+    print()
+    print('p2 from start to (1, 1), should be about 0.0')
+    print('Actually is:', p2.get_distance_from_start())
 
 def run_test_get_distance_traveled():
     """
